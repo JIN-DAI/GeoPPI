14819e8d8b97311501c595d58cf6fd5d08688718
JIN-DAI <daijing491@gmail.com>
2022-01-10 20:26:51 +0800

    more comments

--- gen_interface.py
+++ gen_interface.py
@@ -3,36 +3,39 @@ import sys
 from pymol import cmd
 import InterfaceResidues
 
-pdbobject=  sys.argv[1]
+pdbobject = sys.argv[1]
 namepdb = os.path.basename(pdbobject)
 name = namepdb.split('.')[0]
 
-interface_info=  sys.argv[2]
+interface_info = sys.argv[2]
 chainsAB = interface_info.split('_')
 chainsAB = chainsAB[0]+chainsAB[1]
 
-workdir=  sys.argv[3]
+workdir = sys.argv[3]
 cmd.load(pdbobject)
-interfaces= []
+interfaces = []
 
 for i in range(len(chainsAB)):
 	for j in range(i+1,len(chainsAB)):
-		cha,chb=chainsAB[i],chainsAB[j]
-		if cha==chb:continue
+		cha, chb = chainsAB[i], chainsAB[j]
+		if cha == chb: continue
+		# run script interfaceResidue in pymol to get residues on interface
+		# script outputs results to temp/temp.txt 
 		cmd.do('interfaceResidue {}, chain {}, chain {}'.format(name, cha, chb))
-		mapp = {'chA':cha,'chB':chb}
-		ffile = open('temp/temp.txt','r')
+		# parse output file to list `interfaces`
+		mapp = {'chA':cha, 'chB':chb}
+		ffile = open('temp/temp.txt', 'r')
 		for line in ffile.readlines():
 			linee = line.strip().split('_')
 			resid = linee[0]
 			chainn = mapp[linee[1]]
-			inter='{}_{}_{}_{}'.format(cha,chb,chainn,resid)
+			inter='{}_{}_{}_{}'.format(cha, chb, chainn, resid)
 			if inter not in interfaces:
 				interfaces.append(inter)
 		ffile.close()
 		os.system('rm temp/temp.txt')
 
-ffile = open('{}/interface.txt'.format(workdir),'w')
+ffile = open('{}/interface.txt'.format(workdir), 'w')
 for x in interfaces:
 	ffile.write(x+'\n')
 ffile.close()
