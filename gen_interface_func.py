d5fdfce81bafe4ebee3a58259a90c2b0cd1e7a82
JIN-DAI <daijing491@gmail.com>
2022-01-13 15:52:14 +0800

    cmd.do() -> interfaceResidues()

--- gen_interface_func.py
+++ gen_interface_func.py
@@ -22,19 +22,20 @@ def gen_interface_func(pdbobject, interface_info, workdir):
 			if cha == chb: continue
 			# run script interfaceResidue in pymol to get residues on interface
 			# script outputs results to temp/temp.txt 
-			cmd.do('interfaceResidue {}, chain {}, chain {}'.format(name, cha, chb))
+			#cmd.do('interfaceResidue {}, chain {}, chain {}'.format(name, cha, chb))
+			interfaceResidues(name, cA="c. "+cha, cB="c. "+chb, outputPath=workdir)
 			# parse output file to list `interfaces`
 			mapp = {'chA':cha, 'chB':chb}
-			ffile = open('temp/temp.txt', 'r')
+			ffile = open('{}/temp.txt'.format(workdir), 'r')
 			for line in ffile.readlines():
 				linee = line.strip().split('_')
 				resid = linee[0]
 				chainn = mapp[linee[1]]
 				inter='{}_{}_{}_{}'.format(cha, chb, chainn, resid)
-				if inter not in interfaces:
-					interfaces.append(inter)
+			if inter not in interfaces:
+				interfaces.append(inter)
 			ffile.close()
-			os.system('rm temp/temp.txt')
+			os.remove('{}/temp.txt'.format(workdir))
 
 	ffile = open('{}/interface.txt'.format(workdir), 'w')
 	for x in interfaces:
@@ -43,5 +44,7 @@ def gen_interface_func(pdbobject, interface_info, workdir):
 
 	cmd.save(pdbobject)
 	cmd.delete('all')
+
+	return interfaces
 
 
